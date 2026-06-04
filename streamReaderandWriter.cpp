84c58ba00e4424cc03bced579d55bdef74072adf
Denay2468 <denayaung13@gmail.com>
2026-06-04 15:35:44 +0630

    main function and libraray call

---
+++ streamReaderandWriter.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(){
+    string baris;
+    ofstream outfile;
+    outfile.open("contohfile.txt");
+    cout << ">= Menulis File,\'q\' untuk keluar: " << endl;
+}
