1003f09971fad20e48c8125882bcbc202b4ba1db
Denay2468 <denayaung13@gmail.com>
2026-06-04 15:54:05 +0630

    while loop

--- iofilewithpath.cpp
+++ iofilewithpath.cpp
@@ -12,4 +12,17 @@ int main (){
 
     outfile.open(namaFile + ".txt" , ios::out);
     cout << ">= Menulis File,\'q\' untuk keluar: " << endl;
+
+    while (true){
+
+        cout<<"- ";
+        getline(cin, baris);
+
+        if(baris == "q"){
+
+            break;
+
+        }
+        outfile << baris << endl;
+    }
 }
